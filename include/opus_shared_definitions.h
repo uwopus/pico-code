c7af43b9dfeb3009f8c72dcbb27195c03381237b
johnstonbrendan <brendan.johnston@uwaterloo.ca>
2022-01-15 14:40:08 -0500

    Added encoder mutexes

--- include/opus_shared_definitions.h
+++ include/opus_shared_definitions.h
@@ -5,10 +5,15 @@
 #ifndef OPUS_SHARED_DEFINITIONS_H
 #define OPUS_SHARED_DEFINITIONS_H
 
+#include "pico/sync.h"
+
 
 typedef enum {
     LEFT = 0,
     RIGHT = 1
 }side_t;
+
+mutex_t ENCODER_L_MTX;
+mutex_t ENCODER_R_MTX;
 
 #endif
